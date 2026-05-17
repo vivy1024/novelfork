da8ebee7c28e0ac0ea79a7a7cdc750adc6316892
vivy1024 <vivy1024@example.com>
2026-05-18 06:49:28 +0800

    refactor: 删除废弃的WorkbenchWritingActions组件
    写作工具栏已拆分为各叙述者的AgentQuickActions快捷按钮，
    WorkbenchWritingActions是残留死代码。同时清理WritingWorkbenchRoute
    的writingActions prop和StudioNextApp中的引用。

--- packages/novel-plugin/src/pages/writing-workbench/index.ts
+++ packages/novel-plugin/src/pages/writing-workbench/index.ts
@@ -10,7 +10,6 @@ export * from "./ResourceSaveController";
 export * from "./WorkbenchCanvas";
 export * from "./WorkbenchResourceTree";
 export * from "./WorkbenchTopBar";
-export * from "./WorkbenchWritingActions";
 export * from "./WritingWorkbenchRoute";
 export * from "./resource-viewers";
 export * from "./useWorkbenchResources";
